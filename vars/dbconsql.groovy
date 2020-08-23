f054113fbf2314ce9463db2b6f1048364ccc384b
musthaqeem2012 <40406189+musthaqeem2012@users.noreply.github.com>
2020-08-23 14:16:04 +0530

    Update dbconsql.groovy

--- vars/dbconsql.groovy
+++ vars/dbconsql.groovy
@@ -55,7 +55,7 @@ def dbPassword = ""
 	
 def driver = null;
 	
-	  try {
+	/*  try {
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
@@ -66,7 +66,7 @@ def driver = null;
             println(e.printStackTrace());
             return;
 
-        }
+        }*/
 	
       // Creating a connection to the database
       
